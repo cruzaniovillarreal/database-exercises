9e7b634a5c2c74b3ba6f27f9897de3feddfc61da
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-10-26 15:58:12 -0500

    changed values for database information

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,10 +2,10 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(20) NOT NULL DEFAULT 'Unknown',
-    name VARCHAR(20) NOT NULL DEFAULT 'Unknown',
-    release_date VARCHAR(4),
-    sales DECIMAL(9,2),
+    artist VARCHAR(50) NOT NULL DEFAULT 'Unknown',
+    name VARCHAR(75) NOT NULL DEFAULT 'Untitled',
+    release_date INT UNSIGNED,
+    sales FLOAT UNSIGNED,
     genre VARCHAR(20),
     PRIMARY KEY(id)
 );
