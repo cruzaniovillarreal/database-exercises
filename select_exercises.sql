170e7279d9b20242a485ac901bf80a8d729b04fc
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-10-27 11:19:17 -0500

    Select statements written with captions

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,1 +1,11 @@
 USE codeup_test_db;
+SELECT 'The name of all albums by Pink Floyd.' AS 'INFO';
+SELECT name FROM albums WHERE artist = 'Pink Floyd';
+SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released' AS 'INFO';
+SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT 'The genre for Nevermind' AS 'INFO';
+SELECT genre FROM albums WHERE name = 'Nevermind';
+SELECT 'Which albums were released in the 1990s' AS 'INFO';
+SELECT * FROM albums WHERE release_date >= 1990;
+SELECT 'Which albums had less than 20 million certified sales' AS 'INFO';
+SELECT name FROM albums WHERE sales < 20;
