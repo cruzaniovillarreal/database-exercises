a236ca4f2c1c611ec781314ae074a8acb12f360b
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-10-27 11:57:07 -0500

    Added updates to sale numbers, artist name, and release dates

--- select_exercises.sql
+++ select_exercises.sql
@@ -6,6 +6,6 @@ SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club 
 SELECT 'The genre for Nevermind' AS 'INFO';
 SELECT genre FROM albums WHERE name = 'Nevermind';
 SELECT 'Which albums were released in the 1990s' AS 'INFO';
-SELECT * FROM albums WHERE release_date >= 1990;
+SELECT * FROM albums WHERE release_date BETWEEN 1990 AND 1999;
 SELECT 'Which albums had less than 20 million certified sales' AS 'INFO';
 SELECT name FROM albums WHERE sales < 20;
