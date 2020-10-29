e7bafdf085978383a1612f59c6f98092b8655268
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-10-29 18:15:02 -0500

    Completed subquery bonuses

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -24,3 +24,25 @@ WHERE e.emp_no IN (
     FROM dept_manager dm
     WHERE dm.to_date = '9999-01-01'
     ) AND e.gender = 'f';
+
+SELECT d.dept_name AS DEPARTMENT_NAME
+FROM departments d
+WHERE d.dept_no IN (
+    SELECT dm.dept_no
+    FROM dept_manager dm
+    WHERE dm.emp_no IN (
+        SELECT e.emp_no
+        FROM employees e
+        WHERE e.gender = 'f'
+        ) AND dm.to_date = '9999-01-01'
+)
+
+SELECT e.first_name, e.last_name
+FROM employees e
+WHERE e.emp_no = (
+    SELECT s.emp_no
+    FROM salaries s
+    WHERE s.salary =
+        (SELECT MAX(s.salary)
+            FROM salaries s)
+    );
